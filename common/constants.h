16021b28d9bb402b9e154a0a83155ece498b49fe
AdriaSerraMoral <adriaserra4@gmail.com>
2019-10-18 01:27:34 -0700

    constants - file with constnats

---
+++ common/constants.h
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+///
+///     @file       constants.h
+///     @author     Adria Serra Moral (adriaserra4@gmail.com)
+///     @date       10-16-2019
+///
+///     @brief      This file includes constants that may be common throughout the code 
+///
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+namespace AutoSM {
+namespace constants {
+
+	const double PI = 3.14159265359;
+
+	const double D2R = PI / 180.0;
+
+	const double R2D = 1 / D2R;
+
+	const double MIN_DIVD = 0.00001;
+
+	const double GRAVITY_MAG = 9.79;
+
+}
+}
