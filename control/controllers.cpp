d8fc5d86d69032b200304b5d04c0bb718d53637e
AdriaSerraMoral <adriaserra4@gmail.com>
2019-10-24 23:10:29 -0700

    control/controllers - controller classe

---
+++ control/controllers.cpp
@@ -0,0 +1,172 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+///
+///     @file       controllers.h
+///     @author     Adria Serra Moral (adriaserra4@gmail.com)
+///     @date       10-24-2019
+///
+///     @brief      This file includes all the vehicle controllers to take a rigid-body
+///									to our desired state
+///
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "controllers.h"
+#include "math/transformation.h"
+
+namespace AutoSM { 
+namespace control {
+
+	/**
+	 *	@brief	ControllerBase
+	 *	
+	 *					Constructor, connects pointers and defines number of inputs
+	 */
+	ControllerBase::ControllerBase( const int n_control, VehicleStates* statePtr, TrajectoryCommand* trajPtr, 
+			ControllerStates* ctrlStatePtr ) : 
+		n(n_control), vehicle_states_ptr(statePtr), traj_ptr(trajPtr), ctrl_states_ptr(ctrlStatePtr) {
+
+			// If the pointer for controller state is empty, instantiate
+			if( !ctrl_states_ptr ){
+
+				// instantiate pointer
+				ctrl_states_ptr = new ControllerStates();
+				own_ctrl_state_ptr = true;
+
+			}
+
+			output.resize(n);
+			output.setZero();
+	}
+
+	/**
+	 *	@brief	ControllerBase
+	 *	
+	 *					Destructor, deletes the controller state pointer if it owns it
+	 */
+	ControllerBase::~ControllerBase() {
+
+		if( own_ctrl_state_ptr ){
+			delete ctrl_states_ptr;
+		}
+
+	}
+
+
+	/**
+	 *	@brief	initializeController
+	 *
+	 *					function to initialize each particular controller
+	 */
+	void ControllerBase::initializeController( const RigidBodyParams& rigid_body, const ControllerGains& gains, 
+																			 const DynamicLimits& limits ) {
+
+		this->rigid_body = rigid_body;
+		this->gains = gains;
+		this->limits = limits;
+
+	}
+
+	/**
+	 *	@brief	SaturatePositive
+	 *
+	 *					Function to saturate input if greater than max if control signal acts positive to dynamics
+	 */
+	void saturatePositive ( double& control_input, double& control_max, double& control_min, double& error, int& windup,
+                       bool& is_saturated ) {
+
+		if (control_input >= control_max && error >= 0.0) {
+        windup        = 0;
+        control_input = control_max;
+        is_saturated  = true;
+    } else if (control_input >= control_max && error < 0.0) {
+        windup        = 0;
+        control_input = control_max;
+        is_saturated  = true;
+    } else if (control_input <= control_min && error > 0.0) {
+        windup        = 0;
+        control_input = control_min;
+        is_saturated  = true;
+    } else if (control_input <= control_min && error <= 0.0) {
+        windup        = 0;
+        control_input = control_min;
+        is_saturated  = true;
+    } else {
+        windup = 1;
+    }
+
+	}
+
+	/**
+	 *	@brief	SaturateNegative
+	 *
+	 *					Function to saturate input if greater than max if control signal acts opposite to dynamics
+	 */
+	void saturateNegative ( double& control_input, double& control_max, double& control_min, double& error, int& windup,
+                       bool& is_saturated ) {
+		if (control_input >= control_max && error <= 0.0) {
+        windup        = 0;
+        control_input = control_max;
+        is_saturated  = true;
+    } else if (control_input >= control_max && error > 0.0) {
+        windup        = 0;
+        control_input = control_max;
+        is_saturated  = true;
+    } else if (control_input <= control_min && error < 0.0) {
+        windup        = 0;
+        control_input = control_min;
+        is_saturated  = true;
+    } else if (control_input <= control_min && error >= 0.0) {
+        windup        = 0;
+        control_input = control_min;
+        is_saturated  = true;
+    } else {
+        windup = 1;
+    }
+
+	}
+
+
+	/**
+	 *	@brief	RBDIC
+	 *	
+	 *					Constructor, connects pointers and defines number of inputs
+	 */
+	RBDIC::RBDIC( VehicleStates* statePtr, TrajectoryCommand* trajPtr, 
+			ControllerStates* ctrlStatePtr ) :
+		ControllerBase(6, statePtr, trajPtr) {}
+
+
+	/**
+	 *	@brief	getControl
+	 *
+	 *					get Force and Moment required to move to desired trajectory (linear dynamcis)
+	 */
+	void RBDIC::getControl() {
+
+		Mat3d R_BG = vehicle_states_ptr->quat_BG.toRotationMatrix(); 
+		Vec3d alpha_des_B = R_BG * traj_ptr->alpha_b_G;
+
+		Mat3d w_b_B_skew = math::skewMatrix( vehicle_states_ptr->w_b_B );
+
+		// invert angular dynamics
+		output.segment<3>(0) = rigid_body.inertia * alpha_des_B + w_b_B_skew * ( rigid_body.inertia * vehicle_states_ptr->w_b_B );
+
+		// invert linear dynamics
+		output.segment<3>(3) = rigid_body.mass * (traj_ptr->acc_b_G + rigid_body.drag_damp_coeff * vehicle_states_ptr->vel_b_G);
+
+	}	
+
+	/**
+	 *	@brief	MRAC
+	 *	
+	 *					Constructor, connects pointers and defines number of inputs
+	 */
+	MRAC::MRAC( const int n_control, VehicleStates* statePtr, TrajectoryCommand* trajPtr, 
+			ControllerStates* ctrlStatePtr ) :
+		ControllerBase(n_control, statePtr, trajPtr) {}
+
+
+
+
+}
+}
---
+++ control/controllers.h
@@ -0,0 +1,229 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+///
+///     @file       controllers.h
+///     @author     Adria Serra Moral (adriaserra4@gmail.com)
+///     @date       10-24-2019
+///
+///     @brief      This file includes all the vehicle controllers to take a rigid-body
+///									to our desired state
+///
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#pragma once
+
+#include "common/types.h"
+
+namespace AutoSM { 
+namespace control {
+	
+	
+	/**
+	 *	@brief	ControllerBase
+	 *
+	 *					Base controller class that is inherited and contains all relevant
+	 *					data necessary to implement any particular controller. This way, we
+	 *					provide a common interface
+	 *
+	 */
+	class ControllerBase { 
+	public:
+		EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+		
+		//--------------- Output -----------------------------------------------------
+		VecXd				output;
+
+		/**
+		 *	@brief	ControllerBase
+		 *	
+		 *					Constructor, connects pointers and defines number of inputs
+		 */
+		ControllerBase( const int n_control, VehicleStates* statePtr, TrajectoryCommand* trajPtr, 
+			ControllerStates* ctrlStatePtr = nullptr  );
+		~ControllerBase();
+
+		/**
+		 *	@brief	controlPosition
+		 *
+		 *					get Force required to move to desired trajectory (linear dynamcis)
+		 */
+		virtual void		controlPosition( ) = 0;
+		virtual VecXd		controlPosition( TrajectoryCommand*	traj_ptr ) = 0;
+
+		/**
+		 *	@brief	controlAttitude
+		 *
+		 *					get Moment required to move to desired trajectory (angular dynamcis)
+		 */
+		virtual VecXd		controlAttitude( TrajectoryCommand*	traj_ptr ) = 0;
+		virtual void		controlAttitude(  ) = 0;
+
+		/**
+		 *	@brief	controlVelocity
+		 *
+		 *					get Force required to move to desired trajectory (linear dynamcis)
+		 */
+		virtual void		controlVelocity( ) = 0;
+		virtual VecXd		controlVelocity( TrajectoryCommand*	traj_ptr ) = 0;
+
+
+		/**
+		 *	@brief	getControl
+		 *
+		 *					get Force and Moment required to move to desired trajectory (linear dynamcis)
+		 */
+		virtual VecXd		getControl(	TrajectoryCommand*	traj_ptr ) = 0;
+		virtual void		getControl(	 ) = 0;
+
+		/**
+		 *	@brief	initializeController
+		 *
+		 *					function to initialize each particular controller
+		 */
+		virtual void initializeController( const RigidBodyParams& rigid_body, const ControllerGains& gains, 
+																			 const DynamicLimits& limits ) = 0;
+
+		/**
+		 *	@brief	SaturatePositive
+		 *
+		 *					Function to saturate input if greater than max if control signal acts positive to dynamics
+		 */
+		static void saturatePositive ( double& control_input, double& control_max, double& control_min, double& error, int& windup,
+                         bool& is_saturated );
+
+		/**
+		 *	@brief	SaturateNegative
+		 *
+		 *					Function to saturate input if greater than max if control signal acts opposite to dynamics
+		 */
+		static void saturateNegative ( double& control_input, double& control_max, double& control_min, double& error, int& windup,
+                         bool& is_saturated );
+
+	protected:
+
+		// Rigi Body Model
+		RigidBodyParams			rigid_body;
+
+		// pointer to rigid body state
+		VehicleStates*			vehicle_states_ptr 		{nullptr};
+
+		// pointer to controller states
+		ControllerStates*		ctrl_states_ptr				{nullptr};
+
+		// Input to Controller (From motion Planner) 
+		TrajectoryCommand*	traj_ptr	 						{nullptr};
+
+
+		//---------------- Limits ----------------------------------------------------
+		DynamicLimits				limits;
+
+
+		//---------------- Gains  ----------------------------------------------------
+		ControllerGains			gains;
+
+		// Control properties
+
+		// number of control outputs
+		const int n {0};
+
+		// to know if we need to delete ptr in destructor
+		bool own_ctrl_state_ptr 									{false};
+
+	};
+
+	/**
+	 *	@brief	RBDIC
+	 *
+	 *					Rigid Body Dynamic Inversion Controller (RBDIC)
+	 *					Used rigid-body model with "perfect" Dynamic Inversion to get the control
+	 *					Outputs.
+	 *				
+	 *					Used for simulation, move with true dynamics 
+	 *					6 output (3 Moment, 3 Force)
+	 */	
+	class RBDIC : public ControllerBase {
+	public:
+		EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+
+		RBDIC( VehicleStates* statePtr, TrajectoryCommand* trajPtr, 
+			ControllerStates* ctrlStatePtr = nullptr  );
+		~RBDIC() = default;
+
+		/**
+		 *	@brief	getControl
+		 *
+		 *					get Force and Moment required to move to desired trajectory (linear dynamcis)
+		 */
+		void getControl() override;
+
+
+	};
+
+	/**
+	 *	@brief	MRAC
+	 *
+	 *					Model Reference Adaptive Controller (MRAC)
+	 *					Used a reference model with Dynamic Inversion to get the control
+	 *					Inputs.
+	 *
+	 *					Adaptive Elements can be an Integrator or Network
+	 */
+	class MRAC : public ControllerBase {
+	public:
+		EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+		MRAC( const int n_control, VehicleStates* statePtr, TrajectoryCommand* trajPtr, 
+			ControllerStates* ctrlStatePtr = nullptr  );
+		~MRAC() = default;
+
+		// /**
+		//  *	@brief	controlPosition
+		//  *
+		//  *					get Force required to move to desired trajectory (linear dynamcis)
+		//  */
+		// void		controlPosition( ) override;
+		// VecXd		controlPosition( TrajectoryCommand*	traj_ptr ) override;
+
+		// /**
+		//  *	@brief	controlAttitude
+		//  *
+		//  *					get Moment required to move to desired trajectory (angular dynamcis)
+		//  */
+		// VecXd		controlAttitude( TrajectoryCommand*	traj_ptr ) override;
+		// void		controlAttitude(  ) override;
+
+		// /**
+		//  *	@brief	controlVelocity
+		//  *
+		//  *					get Force required to move to desired trajectory (linear dynamcis)
+		//  */
+		// void		controlVelocity( ) override;
+		// VecXd		controlVelocity( TrajectoryCommand*	traj_ptr ) override;
+
+
+		// /**
+		//  *	@brief	getControl
+		//  *
+		//  *					get Force and Moment required to move to desired trajectory (linear dynamcis)
+		//  */
+		// VecXd		getControl(	TrajectoryCommand*	traj_ptr ) override;
+		// void		getControl(	 ) override;
+
+		// /**
+		//  *	@brief	initializeController
+		//  *
+		//  *					function to initialize each particular controller
+		//  */
+		// void initializeController( const RigidBodyParams& rigid_body, const ControllerGains& gains, 
+		// 																	 const DynamicLimits& limits ) override;
+
+
+
+	};
+
+
+}	// namespace AutoSM
+} // namespace control
